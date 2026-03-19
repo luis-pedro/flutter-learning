3e8ebcb17c4fbc3a47c4f8d90460f82c4781bf06
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-03-19 11:20:20 -0300

    estudando o código

--- fundamentos/flutter_application_1/lib/main.dart
+++ fundamentos/flutter_application_1/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Primeiro App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.red),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hello, World!'),
     );
   }
 }
